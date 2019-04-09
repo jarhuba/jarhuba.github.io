10dfa2ca18c9d5a44e83d22cbc0be78f64ad8baf
jarhuba@gmail.com <Dragon1Gandalf1>
2019-04-09 13:58:13 +0200

    change adress in script.js v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://calm-gorge-24062.herokuapp.com/v1/task/getTasks';
+  var apiRoot = 'https://calm-gorge-24062.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
